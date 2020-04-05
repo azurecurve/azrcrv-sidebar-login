ada1d738628d0b29455753e4333852daf96355dc
azurecurve <github@azurecurve.co.uk>
2020-04-05 14:04:44 +0100

    * Update version number

--- azrcrv-sidebar-login.php
+++ azrcrv-sidebar-login.php
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------------
  * Plugin Name: Sidebar Login
  * Description: Login via AJAX enabled sidebar widget.
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: azurecurve
  * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/sidebar-login/
